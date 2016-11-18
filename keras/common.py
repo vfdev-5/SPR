7ad293cbbab9857d81e9b1d9f0de6cb2210d8321
vfdev-5 <vfdev.5@gmail.com>
2016-11-18 16:49:36 +0000

    * [WIP] a bug found with yearmonth

--- keras/common.py
+++ keras/common.py
@@ -2,7 +2,8 @@
 #
 # Script to load data using Pandas
 #
-
+from math import floor, ceil
+import logging
 import numpy as np
 import pandas as pd
 
@@ -39,13 +40,76 @@ def load_data(filename, yearmonth_start, yearmonth_end):
     return df
 
 
-def clean_data_inplace(df):
+def to_yearmonth(yearmonthdate_str):
+    """
+    Convert '2016-01-23' -> 201601
+    """
+    # yearmonth = int(yearmonth_str[:7].replace('-', ''))
+    yearmonth = int(yearmonthdate_str[:4] + yearmonthdate_str[5:7])
+    return yearmonth
+
+
+def get_nb_months(start_date_str, end_date_str):
+    res = to_yearmonth(end_date_str)*0.01 - to_yearmonth(start_date_str)*0.01
+    return int(res) * 12 + int((res - int(res)) * 100 + 0.5)
+
+
+def minimal_clean_data_inplace(df):
     """
     Script to clean data in input DataFrame
     """
+    # There are some 'unknown' users in train dataset only
+    unknown_users = df['sexo'].isnull() & df['age'].isnull() & df['ind_empleado'].isnull() & \
+                    df['fecha_alta'].isnull() & df['pais_residencia'].isnull()
+
+    logging.info("- Number of unknown clients : %s" % unknown_users.sum())
+
+    # **Remove these users** !
+    df.drop(df[unknown_users].index, inplace=True)
+
+    logging.info("- Number of columns with nan : %s" % df.isnull().any().sum())
+
+    # Remove accent
+    df.loc[df['nomprov'] == "CORU\xc3\x91A, A", "nomprov"] = "CORUNA"
+
+    unknown_cols = ['sexo',
+                    'ind_empleado',
+                    'pais_residencia',
+                    'ult_fec_cli_1t',
+                    'conyuemp',
+                    'canal_entrada',
+                    'nomprov',
+                    'segmento',
+                    'tiprel_1mes',
+                    'indrel_1mes',
+                    'renta']
+    # Start with cols -> replace nan with UNKNOWN
+    for col in unknown_cols:
+        df.loc[df[col].isnull(), col] = "UNKNOWN"
+
+    # Next `fecha_alta` :
+    assert df['fecha_alta'].isnull().sum() == 0, \
+        "Need to replace nan in 'fecha_alta', count=%s" % df['fecha_alta'].isnull().sum()
+
+    # **Remove 'tipodom' and 'cod_prov' columns**
+    df.drop(["tipodom", "cod_prov"], axis=1, inplace=True)
+
+    # Target labels : `ind_nomina_ult1`, `ind_nom_pens_ult1` : nan -> 0
+    # I could try to fill in missing values for products by looking at previous months,
+    # but since it's such a small number of values for now I'll take the cheap way out.
+    df.loc[df.ind_nomina_ult1.isnull(), "ind_nomina_ult1"] = 0
+    df.loc[df.ind_nom_pens_ult1.isnull(), "ind_nom_pens_ult1"] = 0
+
+    # replace 'antiguedad' with the number of months between 'fecha_alta' and 'fecha_dato'
+    def _compute_duration(row):
+        return get_nb_months(row['fecha_alta'], row['fecha_dato'])
+
+    #df['antiguedad'] = df.apply(_compute_duration, axis=1)
+    #df.drop(['antiguedad'], axis=1, inplace=True)
 
 
-def process_data_inplace(df):
+def preprocess_data_inplace(df):
     """
     Script to process data in input DataFrame
     """
+    pass
